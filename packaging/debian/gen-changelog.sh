8978fcf4667df868ffc238a4e1572d1e23521a89
ebasov <eduard.basov@softline.com>
2026-03-06 01:00:13 +0300

    Fix gen changelog

--- packaging/debian/gen-changelog.sh
+++ packaging/debian/gen-changelog.sh
@@ -3,14 +3,13 @@ set -e
 
 name="${1:?package name required}"
 debver="${2:?package version required}"
-
-date_rfc2822="$(LC_ALL=C date -R)"
+date_rfc2822="$(LC_ALL=C date '+%a, %d %b %Y %H:%M:%S %z')"
 
 cat <<EOF
 ${name} (${debver}) unstable; urgency=medium
 
   * Automated build.
 
- -- Eduard Basov <ebasov@ispmanager.ru> ${date_rfc2822}
+ -- Eduard Basov <ebasov@ispmanager.ru>  ${date_rfc2822}
 
 EOF
